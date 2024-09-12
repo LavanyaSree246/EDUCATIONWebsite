f808ce9c6df560c39030003d436748e18a1ef5f2
LavanyaSree246 <sreelavanya246@gmail.com>
2024-09-12 14:09:01 +0530

    Update Contact.jsx

--- src/components/about/contact/Contact.jsx
+++ src/components/about/contact/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
   const map = 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14020.65059791602!2d77.20972969999998!3d28.534830499999984!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x52c2b7494e204dce!2sNew%20Delhi%2C%20Delhi!5e0!3m2!1sen!2sin!4v1726119328959!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" '
   return (
     <>
-      <Back title='Contact us' />
+      <Back title='Contact Us' />
       <section className='contacts padding'>
         <div className='container shadow flexSB'>
           <div className='left row'>
